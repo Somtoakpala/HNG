2e67effbe8fe1ea1552be44ff69183d543ef338c
Akpala Somtochukwu <81123528+Somtoakpala@users.noreply.github.com>
2025-01-31 12:32:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *"); // Enable CORS
 header("Access-Control-Allow-Method: GET"); // Enable GET Method only
- 
+
+echo "Hello World0";
 
 // Set response data
 $response = [
